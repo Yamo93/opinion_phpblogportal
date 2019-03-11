b618880777719b834acb941d5a447cc0f427783d
Yamo Gebrewold <yamo.gebrewold@gmail.com>
2019-03-11 16:40:56 +0300

    small adjustments

--- main.php
+++ main.php
@@ -57,7 +57,7 @@
     </nav>
 
     <!-- Välkomstmeddelande -->
-    <div class="card text-center">
+    <div class="card text-center card-welcome">
         <div class="card-body">
         <h5 class="card-title">Välkommen till <span>Opinion</span>!</h5>
         <p class="card-text">Här får du dela med dig av din åsikt.</p>
@@ -74,6 +74,13 @@
 
         </div>
     </div>
+
+    <footer class="footer">
+        <ul>
+            <li><a href="about.php">Om webbplatsen</a></li>
+            <li><a href="contact.php">Kontakta oss</a></li>
+        </ul>
+    </footer>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
